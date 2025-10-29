3862d0158db8e8ab7bb8209e1930c2d4d16eb116
Vivek Chavan <vivekchavan942@gmail.com>
2025-10-29 15:20:41 +0530

    script edit

--- Backend/config/db.js
+++ Backend/config/db.js
@@ -3,7 +3,7 @@ import mongoose from 'mongoose'
 
  export default async function configDb(){
 	try {
-		await mongoose.connect("mongodb+srv://vivekchavan942_db_user:6CtSpRORNdgTz3HE@onthespotx.fbtmb0x.mongodb.net/?appName=OnTheSpotX")
+		await mongoose.connect(process.env.DB_URL)
 		console.log('Db is connected');
 		
 	} catch (error) {
