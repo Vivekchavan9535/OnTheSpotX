da451219d65d9531da99ae4594303f90039bff37
Vivek Chavan <vivekchavan942@gmail.com>
2025-11-25 16:45:51 +0530

    ++ handling

--- Backend/app/controller/webhook-controller.js
+++ Backend/app/controller/webhook-controller.js
@@ -18,7 +18,8 @@ webhookCtrl.handleWhatsapp = async (req, res) => {
 		await axios.post(webhookUrl, { received: req.body });
 
 		const messageText = (req.body?.data?.body || "").trim().slice(0, 1);
-		const from = "+" + (req.body?.data?.from || "").replace("@c.us", "").trim();
+		const from = ("+" + (req.body?.data?.from || "").replace("@c.us", "").trim()).replace("++", "+");
+
 
 		
 
