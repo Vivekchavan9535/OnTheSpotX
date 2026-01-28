5c15803b39784c845e00d2dbcb3bdebfe98bfbe8
Vivek Chavan <vivekchavan942@gmail.com>
2026-01-29 01:54:16 +0530

    new url for backend server

--- Frontend/src/config/axios.js
+++ Frontend/src/config/axios.js
@@ -1,10 +1,5 @@
 import axios from 'axios'
 export default axios.create({
-<<<<<<< HEAD
-// baseURL:"https://onthespotx.onrender.com",
-	baseURL:"http://localhost:3030"
-=======
-// baseURL:"https://onthespotx.onrender.com", expired
-	baseURL:"https://onthespotx-de8l.onrender.com"
->>>>>>> 7a8e7dd9e52276914010f97cca4ef6487368915e
+	// baseURL:"https://onthespotx.onrender.com", expired
+	baseURL: "https://onthespotx-de8l.onrender.com"
 })
