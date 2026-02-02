3c860db47ee6f26f00eb0dcf32dd42010cd5eaf2
Vivek Chavan <136600536+Vivekchavan9535@users.noreply.github.com>
2026-02-02 07:57:24 +0530

    Update axios baseURL to use the original endpoint

--- Frontend/src/config/axios.js
+++ Frontend/src/config/axios.js
@@ -1,5 +1,5 @@
 import axios from 'axios'
 export default axios.create({
-	// baseURL:"https://onthespotx.onrender.com", expired
-	baseURL: "https://onthespotx-de8l.onrender.com"
+	baseURL:"https://onthespotx.onrender.com", expired
+	// baseURL: "https://onthespotx-de8l.onrender.com"
 })
