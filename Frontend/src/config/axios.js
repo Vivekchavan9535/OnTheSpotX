66094c32440f854c38e433aa6b52ed477c5ec74a
Vivek Chavan <vivekchavan942@gmail.com>
2026-02-03 06:22:53 +0530

    save latest changes

--- Frontend/src/config/axios.js
+++ Frontend/src/config/axios.js
@@ -1,5 +1,6 @@
 import axios from 'axios'
 export default axios.create({
-	// baseURL:"https://onthespotx.onrender.com", expired
-	baseURL: "https://onthespotx-de8l.onrender.com"
+	// baseURL:"https://onthespotx.onrender.com", 
+	// baseURL: "https://onthespotx-de8l.onrender.com,
+	baseURL: "http://localhost:3030"
 })
