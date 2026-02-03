326a99602ee9dc53584520d8506b714740a94356
Vivek Chavan <136600536+Vivekchavan9535@users.noreply.github.com>
2026-02-03 06:24:30 +0530

    Uncomment baseURL for production environment

--- Frontend/src/config/axios.js
+++ Frontend/src/config/axios.js
@@ -1,11 +1,6 @@
 import axios from 'axios'
 export default axios.create({
 <<<<<<< HEAD
-	// baseURL:"https://onthespotx.onrender.com", 
+	baseURL:"https://onthespotx.onrender.com", 
 	// baseURL: "https://onthespotx-de8l.onrender.com,
-	baseURL: "http://localhost:3030"
-=======
-	baseURL:"https://onthespotx.onrender.com"
-	// baseURL: "https://onthespotx-de8l.onrender.com"
->>>>>>> f89f95438d0a7be051ecafcd2c1ac22c7aaf0b8b
 })
